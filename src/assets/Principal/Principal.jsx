00b076dbdaebcb829a5fc2efc6773914cef6c7d1
AnabellaCarballo <anabellac698@gmail.com>
2024-06-05 18:00:28 -0300

    nueva pantalla principal

--- src/assets/Principal/Principal.jsx
+++ src/assets/Principal/Principal.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
-import CartasEntrada from './CartasEntrada'
-import "../Estilos/Principal.css"
+import React from 'react';
+import CartasEntrada from './CartasEntrada';
+import "../Estilos/Principal.css";
 
 export const Principal = () => {
   return (
-    <>
+    <> 
+    
+
       <div className='cartas-unidas'>
-        <CartasEntrada  titulo='Administrar Cliente' url='/FormularioCliente/RegistroCliente' />
-        <CartasEntrada titulo='Administrar Proveedor'/>
-        <CartasEntrada titulo='Administrar Producto'/>
+        <CartasEntrada 
+          titulo='Administrar Cliente' 
+          url='/FormularioCliente/RegistroCliente' 
+          image="/src/images/cliente.png" 
+          alt="Imagen de cliente" 
+        />
+        <CartasEntrada 
+          titulo='Administracion ' 
+          image="/src/images/admin.png" 
+          alt="Imagen de administracion" 
+        />
+        <CartasEntrada 
+          titulo='Administrar stock' 
+          image="/src/images/stock1.png" 
+          alt="Imagen de stock" 
+        />
       </div>
 
       <div className='cartas-unidas'>
-        <CartasEntrada titulo='Administrar Venta'/>
-        <CartasEntrada titulo='Administrar Materia Prima'/>
-        <CartasEntrada titulo='Administrar Stock '/>
+        <CartasEntrada 
+          titulo=' Venta' 
+          image="/src/images/ventas.png" 
+          alt="Imagen de venta" 
+        />
+        <CartasEntrada 
+          titulo='Administrar Proveedor' 
+          image="/src/images/proveedor.png" 
+          alt="Imagen de proveedor" 
+        />
+        <CartasEntrada 
+          titulo='RR.HH' 
+          image="/src/images/rrhh.png" 
+          alt="Imagen de rr.hh" 
+        />
       </div>
+      
     </>
-  )
-}
+  );
+};
