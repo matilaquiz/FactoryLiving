929bd5bf54ef565b654fcb768b1dba7e3fa62dbd
Cayo Yahir Zerda Gonzales <cayoyahirgonzales@gmail.com>
2024-06-24 20:57:47 -0300

    nuevo Menu

--- src/assets/FormularioCliente/Menu.jsx
+++ src/assets/FormularioCliente/Menu.jsx
@@ -3,34 +3,24 @@ import "../Estilos/Menu.css"
 // import equiz from "../imagenes/borrar.png"
 // import {useState} from "react";
 
-
 export const Menu = () => {
-
-  // const abrir_cerrar_menu = () =>{
-  //   const [isOpen, setIsOpen] = useState(false)
-  // }
   return (
-        <nav className="navegador">
-
-          {/* <div className="nav-hambur" >
-            <img src={menu} onClick={()=> setIsOpen(!isOpen)} />
-          </div> */}
-
-          <div id="Menu" >
-            {/* <img src={equiz} className={"Menu ${isOpen && open}"} /> */}
-            <ul>
-              <li><a href="#">Inicio</a></li>
-              <li><a href="#">Nosotros</a></li>
-              <li><a href="#">Servicios</a></li>
-              <li><a href="#">Productos</a></li>
-              <li><a href="#">Promociones</a></li>
-              <li><a href="">Contacto</a></li>
-            </ul>
-          </div>
-          
-          
-          <div className="nav-logo">FACTORYLIVING.APP</div>
-        </nav>
+      <nav className="navegador">
+        <div className="nav-logo">
+          FACTORYLIVING
+        </div>
+        <div className="Menu">
+          <img src="/src/Images/admin.png"/>
+          <ul className="ul-Menu">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Promociones</a></li>
+            <li><a href="">Contacto</a></li>
+          </ul>
+        </div>    
+      </nav>
   )
 }
 
