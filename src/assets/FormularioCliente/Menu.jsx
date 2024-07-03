562f3a3923fc40f0489c3df40abdb3e178b27a2e
Cayo Yahir Zerda Gonzales <cayoyahirgonzales@gmail.com>
2024-07-03 17:34:52 -0300

    ultimos cambios

--- src/assets/FormularioCliente/Menu.jsx
+++ src/assets/FormularioCliente/Menu.jsx
@@ -8,13 +8,13 @@ export const Menu = () => {
         </div>
         <div className="Menu">
           <ul className="ul-Menu">
-            <li><a href="#"><img src="/src/Images/casa-icono-silueta.png"/>Inicio</a></li>
-            <li><a href="#"><img src="/src/Images/nueva-cuenta.png"/>Cliente</a></li>
+            <li><a href="/"><img src="/src/Images/casa-icono-silueta.png"/>Inicio</a></li>
+            <li><a href="/FormularioCliente/RegistroCliente"><img src="/src/Images/nueva-cuenta.png"/>Cliente</a></li>
             <li><a href="#"><img src="/src/Images/sobre-nosotros.png"/>Nosotros</a></li>
-            <li><a href="#"><img src="/src/Images/paquete-o-empaquetar.png"/>Stock</a></li>
-            <li><a href="#"><img src="/src/Images/ventas2.png"/>Venta</a></li>
+            <li><a href="/indexStock/Stock"><img src="/src/Images/paquete-o-empaquetar.png"/>Stock</a></li>
+            <li><a href="/VentasSillones/VentasGUI"><img src="/src/Images/ventas2.png"/>Venta</a></li>
             <li><a href="#"><img src="/src/Images/promociones.png"/>Promos</a></li>
-            <li><a href=""><img src="/src/Images/recursos-humanos.png"/>R.R.H.H</a></li>
+            <li><a href="#"><img src="/src/Images/recursos-humanos.png"/>R.R.H.H</a></li>
           </ul>
         </div>    
       </nav>
